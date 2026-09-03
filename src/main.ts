1918c5dc4f79d89957fdccdb3a04b0246546cade
Thafsi PV <pvthafsi@gmail.com>
2026-09-03 09:18:34 +0530

    restore cookieParser for auth

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   // Enable CORS for frontend dev server
   app.enableCors({
